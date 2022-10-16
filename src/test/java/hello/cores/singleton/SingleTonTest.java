3d76ff1146fe13fa90cb424bce9b596de7ffff7e
KIM HYUN GI <rlagusrl928@gmail.com>
2022-10-16 18:55:11 +0900

    Day8
    https://deadwhale.notion.site/Day8-ffc2022d298147178a00da8bf8678744

--- src/test/java/hello/cores/singleton/SingleTonTest.java
+++ src/test/java/hello/cores/singleton/SingleTonTest.java
@@ -2,8 +2,11 @@ package hello.cores.singleton;
 
 import hello.cores.AppConfig;
 import hello.cores.member.MemberService;
+import hello.cores.member.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,5 +40,21 @@ public class SingleTonTest {
 //        singleTonService2 = hello.cores.singleton.SingleTonService@c0c2f8d
 
         assertThat(singleTonService1).isSameAs(singleTonService2);
+    }
+
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService ms1 = ac.getBean("memberService", MemberServiceImpl.class);
+        MemberService ms2 = ac.getBean("memberService", MemberServiceImpl.class);
+
+        System.out.println("ms1 = " + ms1);
+        System.out.println("ms2 = " + ms2);
+
+        assertThat(ms1).isSameAs(ms2);
+
     }
 }
